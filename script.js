75f0dc5130646e50e30ba078a0e8642d7c6bae4a
Jack Sloan <jack12sloan@gmail.com>
2022-10-13 13:09:41 -0400

    Create function for one round of rps

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ function getComputerChoice() {
     }
     return choice;
 }
+
+function rpsRound(){
+    const compChoice = getComputerChoice();
+    const userChoice = prompt('R, P, or S');
+    let result;
+    if (userChoice.toUpperCase() === 'R') {
+        if (compChoice === 'Rock') {
+            result = 'tie!';
+        } else if (compChoice === 'Paper'){
+            result = 'lose!';
+        } else if (compChoice === 'Scissors'){
+            result = 'win!';
+        }
+        console.log(`${compChoice}! You ${result}`)
+    } else if (userChoice.toUpperCase() === 'P') {
+        if (compChoice === 'Rock') {
+            result = 'win!';
+        } else if (compChoice === 'Paper'){
+            result = 'tie!';
+        } else if (compChoice === 'Scissors'){
+            result = 'lose!';
+        }
+        console.log(`${compChoice}! You ${result}`)
+        
+    } else if (userChoice.toUpperCase() === 'S'){
+        if (compChoice === 'Rock') {
+            result = 'lose!';
+        } else if (compChoice === 'Paper'){
+            result = 'win!';
+        } else if (compChoice === 'Scissors'){
+            result = 'tie!';
+        }
+        console.log(`${compChoice}! You ${result}`)
+    } else {
+        alert('please choose either "R", "P", or "S"');
+        rpsRound();
+    }
+    return result;
+}
