c90f0c199ec9f0ae0bafa5614ca17400006e2b06
Jack Sloan <jack12sloan@gmail.com>
2022-10-17 14:15:10 -0400

    Add score counter div

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const paper = document.getElementById('Paper');
 const scissors = document.getElementById('Scissors');
 
 const resultDiv = document.getElementById('Result');
+const scoreDiv = document.getElementById('Score');
 
 rock.addEventListener('click', rpsRound);
 paper.addEventListener('click', rpsRound);
 scissors.addEventListener('click', rpsRound);
 
+let userScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const randNum = Math.floor(Math.random() * 3);
@@ -34,29 +37,37 @@ function rpsRound(e){
             result = 'tie!';
         } else if (compChoice === 'Paper'){
             result = 'lose!';
+            computerScore++;
         } else if (compChoice === 'Scissors'){
             result = 'win!';
+            userScore++;
         }
-        resultDiv.textContent = `${compChoice}! You ${result}`
+        resultDiv.textContent = `${compChoice}! You ${result}`;
+        scoreDiv.textContent = `You: ${userScore} | Computer: ${computerScore}`;
     } else if (userChoice.toUpperCase() === 'P') {
         if (compChoice === 'Rock') {
             result = 'win!';
+            userScore++;
         } else if (compChoice === 'Paper'){
             result = 'tie!';
         } else if (compChoice === 'Scissors'){
             result = 'lose!';
+            computerScore++;
         }
         resultDiv.textContent = `${compChoice}! You ${result}`
-        
+        scoreDiv.textContent = `You: ${userScore} | Computer: ${computerScore}`;
     } else if (userChoice.toUpperCase() === 'S'){
         if (compChoice === 'Rock') {
             result = 'lose!';
+            computerScore++;
         } else if (compChoice === 'Paper'){
             result = 'win!';
+            userScore++;
         } else if (compChoice === 'Scissors'){
             result = 'tie!';
         }
         resultDiv.textContent = `${compChoice}! You ${result}`
+        scoreDiv.textContent = `You: ${userScore} | Computer: ${computerScore}`;
     } 
     return result;
 }
@@ -70,3 +81,4 @@ function findUserChoice(e){
         return 'S';
     }
 }
+
